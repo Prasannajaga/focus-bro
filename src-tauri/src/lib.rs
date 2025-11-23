e8e3a579351fd37cb0b9750c983927ba13c6575d
Prasanna <prasannajaga9@gmail.com>
2025-11-23 22:59:31 +0530

    build optimization

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,9 @@
 mod tools;
 
-use tauri_plugin_autostart::MacosLauncher;
+use tauri_plugin_autostart::{MacosLauncher, ManagerExt};
 use tauri_plugin_clipboard_manager;
-use tauri_plugin_global_shortcut::{GlobalShortcutExt };
-use tools::{register_shortcut, read_file , TARGET_KEYS};
- 
+use tauri_plugin_global_shortcut::GlobalShortcutExt;
+use tools::{read_file, register_shortcut, TARGET_KEYS};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -13,24 +12,23 @@ pub fn run() {
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_autostart::init(
             MacosLauncher::LaunchAgent,
-            Some(vec!["--flag1", "--flag2"])
+            Some(vec!["--flag1", "--flag2"]),
         ))
-        .setup(|app | { 
+        .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
                         .level(log::LevelFilter::Info)
                         .build(),
                 )?;
-            }  
+            }
 
-            app.global_shortcut().register(TARGET_KEYS())?; 
+            let _ = app.autolaunch().enable();
+
+            app.global_shortcut().register(TARGET_KEYS())?;
             Ok(())
         })
-        .invoke_handler(tauri::generate_handler![
-            read_file,
-            open_url
-        ])
+        .invoke_handler(tauri::generate_handler![read_file, open_url])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
